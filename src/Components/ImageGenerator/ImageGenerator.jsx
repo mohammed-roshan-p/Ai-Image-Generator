ca64fc518f1a9b82377b142c3564423f93af8db3
Mohammed Roshan P <125496469+mohammed-roshan-p@users.noreply.github.com>
2024-09-06 11:17:53 +0530

    Fetching details added

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
-import './ImageGenerator.css'
-import default_image from '../Assets/default_image.svg'
-const ImageGenerator = () => {
-  return (
-    <div className='ai-image-generator'>
-        <div class="header">
-            AI IMAGE <span>GENERATOR</span>
-        </div>
-        <div class="img-loading">
-            <div class="image">
-                <img src={default_image} alt="Image of a Lady using AI" />
+    import React, { useRef, useState } from 'react'
+    import './ImageGenerator.css'
+    import default_image from '../Assets/default_image.svg'
+    const ImageGenerator = () => {
+        const [image_url,setImage_url] = useState("/");
+        let inputRef = useRef(null) ; 
+        const imageGenerator = async () =>
+        {
+            if(inputRef.current.value === "")
+            {
+                return 0;
+            }
+            const response = await fetch(
+                "https://api.openai.com/v1/images/generations",
+                {
+                    method : "POSt" ,
+                    headers : {
+                        "Content-Type" : "application/json" , 
+                        Authorization : 
+                        "Bearer YOUR_API_KEY" ,
+                        "User-Agent" : "Chrome" , 
+                    },
+                    body: JSON.stringify(
+                        {
+                            prompt : `${inputRef.current.value}` , 
+                            n : 1 , 
+                            size : "512x512"
+                        }
+                    )
+                }
+            );
+            let data = await response.json();
+            console.log(data);
+            let data_array = data.data ; 
+            setImage_url(data_array[0].url);
+        }
+    return (
+        <div className='ai-image-generator'>
+            <div class="header">
+                AI IMAGE <span>GENERATOR</span>
+            </div>
+            <div class="img-loading">
+                <div class="image">
+                    <img src={ image_url === '/' ? default_image : image_url } alt="Image of a Lady using AI" />
+                </div>
+            </div>
+            <div class="search-box">
+                <input type='text' ref={inputRef} className='search-input' placeholder='Describe Your Image !! '/>
+                <div class="submit-button" onClick={() => {imageGenerator()}}>
+                    Generate
+                </div>
             </div>
         </div>
-        <div class="search-box">
-            <input type='text' className='search-input' placeholder='Describe Your Image !! '/>
-            <div class="submit-button">
-                Generate
-            </div>
-        </div>
-    </div>
-  )
-}
+    )
+    }
 
-export default ImageGenerator
+    export default ImageGenerator
